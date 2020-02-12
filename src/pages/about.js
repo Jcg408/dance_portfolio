31a44b5b200d3f7aa52b9f18420210441c5a8a8d
Jcg408 <jcg321@comcast.net>
2020-02-12 17:15:18 -0500

    set up about page for bio information

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Bio from "../components/bio"
+
+const About = () => (
+  <Layout>
+    <SEO title="Bio" />
+    <Bio/>
+  </Layout>
+)
+
+export default About
